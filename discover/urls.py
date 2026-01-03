a6a79244674c1b76bcb6999bae065a18658d82c2
workflow5555 <across.all@outlook.com>
2026-01-03 16:35:57 -0600

    advance_project

--- discover/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-import views
-
-
-urlpatterns = [
-    path('',views.view_discover,name='view_discover'),
-]
--- discover/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def view_discover(request):
-    pass
